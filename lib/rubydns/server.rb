ce31aa92c48cbc32aa136ea5723eeea23ac5b371
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-01-19 02:57:08 +1300

    require 'fiber' for compatibility.

--- lib/rubydns/server.rb
+++ lib/rubydns/server.rb
@@ -18,6 +18,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+require 'fiber'
+
 require 'rubydns/transaction'
 require 'rubydns/extensions/logger'
 
