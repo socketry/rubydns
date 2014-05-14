4ecef48c6d7649898a9cbeab093f95563832eb1e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-05-14 13:35:48 +1200

    List of domain names inline with test class.

--- test/test_resolver_performance.rb
+++ test/test_resolver_performance.rb
@@ -29,7 +29,58 @@ require 'benchmark'
 
 class ResolverPerformanceTest < MiniTest::Test
 	# The larger this number, the better RubyDNS::Resolver will look as it is highly asynchronous.
-	DOMAINS = DATA.readlines.collect{|line| line.chomp!}.flatten
+	DOMAINS = %W{
+		Facebook.com
+		Twitter.com
+		Google.com
+		Youtube.com
+		Wordpress.org
+		Adobe.com
+		Blogspot.com
+		Wikipedia.org
+		Linkedin.com
+		Wordpress.com
+		Yahoo.com
+		Amazon.com
+		Flickr.com
+		Pinterest.com
+		Tumblr.com
+		W3.org
+		Apple.com
+		Myspace.com
+		Vimeo.com
+		Microsoft.com
+		Youtu.be
+		Qq.com
+		Digg.com
+		Baidu.com
+		Stumbleupon.com
+		Addthis.com
+		Statcounter.com
+		Feedburner.com
+		TradeMe.co.nz
+		Delicious.com
+		Nytimes.com
+		Reddit.com
+		Weebly.com
+		Bbc.co.uk
+		Blogger.com
+		Msn.com
+		Macromedia.com
+		Goo.gl
+		Instagram.com
+		Gov.uk
+		Icio.us
+		Yandex.ru
+		Cnn.com
+		Webs.com
+		Google.de
+		T.co
+		Livejournal.com
+		Imdb.com
+		Mail.ru
+		Jimdo.com
+	}
 	
 	def test_resolvers
 		rubydns_resolved = {}
@@ -69,55 +120,3 @@ class ResolverPerformanceTest < MiniTest::Test
 		end
 	end
 end
-
-__END__
-Facebook.com
-Twitter.com
-Google.com
-Youtube.com
-Wordpress.org
-Adobe.com
-Blogspot.com
-Wikipedia.org
-Linkedin.com
-Wordpress.com
-Yahoo.com
-Amazon.com
-Flickr.com
-Pinterest.com
-Tumblr.com
-W3.org
-Apple.com
-Myspace.com
-Vimeo.com
-Microsoft.com
-Youtu.be
-Qq.com
-Digg.com
-Baidu.com
-Stumbleupon.com
-Addthis.com
-Statcounter.com
-Feedburner.com
-TradeMe.co.nz
-Delicious.com
-Nytimes.com
-Reddit.com
-Weebly.com
-Bbc.co.uk
-Blogger.com
-Msn.com
-Macromedia.com
-Goo.gl
-Instagram.com
-Gov.uk
-Icio.us
-Yandex.ru
-Cnn.com
-Webs.com
-Google.de
-T.co
-Livejournal.com
-Imdb.com
-Mail.ru
-Jimdo.com
