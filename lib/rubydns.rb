c5ec2fb9948e57707fad548cc93083440a215b1e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-16 14:48:50 +1300

    Add compatibilty for old array of interfaces.

--- lib/rubydns.rb
+++ lib/rubydns.rb
@@ -9,6 +9,7 @@ require "async/dns"
 require_relative "rubydns/version"
 require_relative "rubydns/server"
 
+# @namespace
 module RubyDNS
 	# Backwards compatibility:
 	Resolver = Async::DNS::Resolver
@@ -25,7 +26,17 @@ module RubyDNS
 	end
 	
 	# @deprecated Use {RubyDNS.run} instead.
-	def self.run_server(...)
-		self.run(...)
+	def self.run_server(*arguments, **options, &block)
+		if arguments.first.is_a?(Array)
+			warn "Using an array of interfaces is deprecated. Please use `Async::DNS::Endpoint` instead.", uplevel: 1
+			
+			endpoints = arguments[0].map do |specification|
+				IO::Endpoint.public_send(*specification)
+			end
+			
+			arguments[0] = IO::Endpoint.composite(*endpoints)
+		end
+		
+		self.run(*arguments, **options, &block)
 	end
 end
